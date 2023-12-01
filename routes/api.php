af08ff8cbcabef21e4d5eef4ee80f8a5e4af5f15
buba1477 <abazov1477@gmail.com>
2023-12-01 15:43:37 +0300

    lv-2

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-//Route::group(['namespace' => 'App\Http\Controllers\Person','prefix' => 'people'], function (){
-//    Route::post('/', 'StoreController');
-//    Route::get('/', 'IndexController');
-//});
+Route::group(['namespace' => 'App\Http\Controllers\Person','prefix' => 'people'], function (){
+    Route::post('/', 'StoreController');
+    Route::get('/', 'IndexController');
+});
 
 
 
